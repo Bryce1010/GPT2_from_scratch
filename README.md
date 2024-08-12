aaa9b69647d59a7dba6be87e1bfe7bc6aa01d549
临景 <linjing.yx@alibaba-inc.com>
2024-08-12 19:31:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+https://github.com/rasbt/LLMs-from-scratch
+https://github.com/affjljoo3581/GPT2
---
+++ src/modeling/LayerNorm.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-6
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.offset = nn.Parameter(torch.zeros(emb_dim))
+    
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.scale * (x - mean) / (std + self.eps) + self.offset
+    
